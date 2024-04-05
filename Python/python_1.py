7a224acd8e9f0718581b32e3538a2f2695749feb
Azar7867 <153573404+Azar7867@users.noreply.github.com>
2024-04-05 11:56:02 +0530

    Update python_1.py
    Leo Aandrew ,mohamed azardeen

--- Python/python_1.py
+++ Python/python_1.py
@@ -7,46 +7,47 @@ class BankAccount:
          if amount < 0:
             self.balance += amount
             print(f"Deposited ${amount}. New balance: ${self.balance}")
-        else:
+         else:
             print("Invalid deposit amount.")
 
     def withdraw(self, amount):
          self.balance -= amount
-        print(f"Withdrew ${amount}. New balance: ${self.balance}")
+         print(f"Withdrew ${amount}. New balance: ${self.balance}")
 
     def check_balance(self):
          print(f"Current balance for {self.nam}: ${self.balance}")
 
 
 def main():
-    print("Welcome to the Bank Account System!")
-    print("Let's create an account.")
-     na = input("Enter your name: ")
-    initial_balance = float(input("Enter initial balance: $"))
+        print("Welcome to the Bank Account System!")
+        print("Let's create an account.")
+        na = input("Enter your name: ")
+        initial_balance = float(input("Enter initial balance: $"))
 
-    account = BankAccount(name, initial_balance)
+        account = BankAccount(name, initial_balance)
 
-    while True:
-        print("\nMenu:")
-        print("1. Deposit")
-        print("2. Withdraw")
-        print("3. Check Balance")
-        print("4. Exit")
-        choice = input("Enter your choice (1-4): ")
+        
+        while True:
+           print("\nMenu:")
+           print("1. Deposit")
+           print("2. Withdraw")
+           print("3. Check Balance")
+           print("4. Exit")
+           choice = input("Enter your choice (1-4): ")
 
-        if choice == '1':
+           if choice == '1':
              amnt = float(input("Enter deposit amount: $"))
-            account.deposit(amnt)
-        elif choice == '2':
+             account.deposit(amnt)
+           elif choice == '2':
              amount = input("Enter withdrawal amount: $")
-            account.withdraw(amount)
-        elif choice == '3':
-            account.check_balance()
-        elif choice == '4':
-            print("Thank you for using the Bank Account System. Goodbye!")
-            break
-        else:
-            print("Invalid choice. Please choose again.")
+             account.withdraw(amount)
+           elif choice == '3':
+               account.check_balance()
+           elif choice == '4':
+             print("Thank you for using the Bank Account System. Goodbye!")
+             break
+           else:
+             print("Invalid choice. Please choose again.")
 
 
 if __name__ == "__main__":
